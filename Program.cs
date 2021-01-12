cc5ce5bac6e9bfac8e67e45032e5597d6ab5c40b
Josh Mann <jlmann72@gmail.com>
2021-01-12 09:15:38 -0500

    TODO: research sorting with IComparer c#

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LargestElements
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public static List<int> Largest(int n, List<int> xs)
+        {
+            // Find the n highest elements in a list
+            if (n == 0)
+            {
+                return new List<int>();
+            }
+            else
+            {
+                // Need to figure out comparison in C# List
+
+
+                new List<int> sortedArray = xs.Sort()
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+            Console.WriteLine(Largest(0, new List<int> { 1, 2, 3, 4, 5 }));
+        }
     }
-  }
 }
